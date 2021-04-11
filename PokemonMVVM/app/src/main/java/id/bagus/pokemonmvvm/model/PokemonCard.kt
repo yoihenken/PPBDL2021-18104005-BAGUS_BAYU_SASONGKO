27eb3b93b6d002170e495333bfe7ef516396b709
yoihenken <18104005@ittelkom-pwt.ac.id>
2021-04-11 21:57:45 +0700

    add model

--- PokemonMVVM/app/src/main/java/id/bagus/pokemonmvvm/model/PokemonCard.kt
+++ PokemonMVVM/app/src/main/java/id/bagus/pokemonmvvm/model/PokemonCard.kt
@@ -1,17 +1,23 @@
 package id.bagus.pokemonmvvm.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@Entity
 data class PokemonCard(
     var name: String?,
 
     @SerializedName("imageUrl")
     var image: String?,
     var rarity: String?,
-    var series: String?
+    var series: String?,
+
+    @PrimaryKey
+    var id: String
 ) : Parcelable {
     data class PokemonCardResponse(
         var cards: MutableList<PokemonCard>
--- PokemonMVVM/app/src/main/java/id/bagus/pokemonmvvm/model/PokemonSet.kt
+++ PokemonMVVM/app/src/main/java/id/bagus/pokemonmvvm/model/PokemonSet.kt
@@ -1,12 +1,18 @@
 package id.bagus.pokemonmvvm.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class PokemonSet(
     var name: String,
 
     @SerializedName("logoUrl")
-    var logo: String
+    var logo: String,
+
+    @PrimaryKey
+    var code: String
 ) {
     data class PokemonSetResponse(
         var sets: MutableList<PokemonSet>
